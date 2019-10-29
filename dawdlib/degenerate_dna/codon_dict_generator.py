e1216414c7c503e3f4d2031866113ee03484b013
shayho <shayho>
2019-10-29 11:18:21 +0200

    1. Added setup.py. 2. Formatted files using black. 3. Added helper functions to generate codon dictionary and graph

---
+++ dawdlib/degenerate_dna/codon_dict_generator.py
@@ -0,0 +1,22 @@
+import pickle
+import typing as tp
+
+from dawdlib.degenerate_dna.codon_selector import (
+    CodonSelector,
+    _remove_duplicate_codons,
+)
+
+
+def generate_codon_dict(organism_id: str, amino_acids: tp.List[str]) -> tp.Dict:
+    cdn_sel = CodonSelector(organism_id)
+    codons = cdn_sel._gen_degenerate_codons(amino_acids)
+    codons = _remove_duplicate_codons(codons)
+    return dict(("".join(sorted(codon["encoded_acids"])), codon) for codon in codons)
+
+
+def write_codon_dict(
+    organism_id: str, amino_acids: tp.List[str], filename: str
+) -> None:
+    codon_dict = generate_codon_dict(organism_id, amino_acids)
+    with open(filename, "wb") as outf:
+        pickle.dump(codon_dict, outf)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dawdlib",
+    version="0.0.1a",
+    url="https://github.com/Fleishman-Lab/dawdlib.git",
+    description="Utils to help create a cost sensitive degenerate codon sequence",
+    packages=find_packages(),
+)
