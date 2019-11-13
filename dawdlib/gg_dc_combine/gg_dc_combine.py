e53f7abfc911ef686b08dd79324415d681e37719
jonathaw <jonathan.weinstein2012@gmail.com>
2019-11-13 09:15:01 +0200

    gg_dc_combine is the module that takes the DC and GG tables and creates all the required oligos

---
+++ dawdlib/gg_dc_combine/gg_dc_combine.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from itertools import product
+from typing import List, Tuple
+import numpy as np
+
+from dawdlib.golden_gate.utils import find_dna_var_poss, parse_dna, parse_resfile
+
+def parse_gg_segments_csv(csv_file: str) -> pd.DataFrame:
+    return pd.read_csv(csv_file, index_col=False)
+
+
+def parse_degenerate_codon_csv(csv_file: str) -> pd.DataFrame:
+    return pd.read_csv(
+        csv_file,
+        index_col=False,
+        na_values="NaN",
+        converters={
+            "ENCODED_AAS": lambda x: x.strip("[]").replace("'", "").split(", "),
+            "ENCODED_COUNT": lambda x: [
+                int(a) for a in x.strip("[]").replace("'", "").split(", ")
+            ],
+        },
+    )
+
+
+def find_oligos(gg_df: pd.DataFrame) -> List[Tuple[int, int]]:
+    oligos: List[Tuple[int, int]] = []
+    for ind1, ind2 in zip(gg_df.index[:-1], gg_df.index[1:]):
+        oligos.append((gg_df.loc[ind1, "idx"], gg_df.loc[ind2, "idx"] - 1))
+    return oligos
+
+
+def find_codons_for_oligo(
+    oligo: Tuple[int, int], dc_df: pd.DataFrame
+) -> List[List[Tuple[int, str]]]:
+
+    oligo_codons: List[List[Tuple[int, str]]] = []
+    sub_df = dc_df.loc[
+        dc_df["DNA_POS"].between(oligo[0], oligo[1]),
+        ["DNA_POS"] + [c for c in dc_df.columns if c.startswith("AMBIGUOUS_CODONS")],
+    ]
+    pos_codon: List[List[Tuple[int, str]]] = []
+    for _, row in sub_df.iterrows():
+        pos_codon.append([])
+        for col in sub_df.columns[1:]:
+            if row[col] is np.nan:
+                continue
+            pos_codon[-1].append((row["DNA_POS"], row[col]))
+
+    for prod in product(*pos_codon):
+        combination: List[Tuple[int, str]] = []
+        for (pos, codon) in prod:
+            combination.append((pos, codon))
+        oligo_codons.append(combination)
+    return oligo_codons
+
+def create_dc_oligo(dna: str, pos_codons: List[Tuple[int, str]], oligo: Tuple[int, int]) -> str:
+    dna_copy = dna
+    for (pos, codon) in pos_codons:
+        dna_copy = dna_copy[:pos] + codon + dna_copy[pos+4:]
+    return dna_copy[oligo[0]: oligo[1]+3]
+
+
+def create_all_dc_oligos(dna: str, gg_df: pd.DataFrame, dc_df: pd.DataFrame):
+    oligos = find_oligos(gg_df)
+    for oligo in oligos:
+        oligo_codons = find_codons_for_oligo(oligo, dc_df)
+        for oligo_codon in oligo_codons:
+            oligo_dna = create_dc_oligo(dna, oligo_codon, oligo)
+            print(oligo, oligo_codon, oligo_dna)
---
+++ dawdlib/gg_dc_combine/tests/test_gg_dc_combine.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import os
+import unittest
+
+from dawdlib.gg_dc_combine.gg_dc_combine import (create_all_dc_oligos,
+                                                 create_dc_oligo,
+                                                 find_codons_for_oligo,
+                                                 find_oligos,
+                                                 parse_degenerate_codon_csv,
+                                                 parse_gg_segments_csv)
+from dawdlib.golden_gate.utils import (find_dna_var_poss, parse_dna,
+                                       parse_resfile)
+
+
+class GGDCCombneTest(unittest.TestCase):
+    def setUp(self):
+        self.here = os.path.dirname(__file__)
+        self.gg_df = parse_gg_segments_csv(f"{self.here}/gg_segments.csv")
+        self.dc_df = parse_degenerate_codon_csv(f"{self.here}/921.csv")
+        self.dna = parse_dna(f"{self.here}/921_DNA_seq")
+
+    def tearDown(self):
+        pass
+
+    def test_parse_gg_segments_csv(self):
+        self.assertEqual(self.gg_df.iloc[0, 0], -1)
+        self.assertEqual(self.gg_df.iloc[0, 1], "src")
+
+        self.assertEqual(self.gg_df.iloc[1, 0], 79)
+        self.assertEqual(self.gg_df.iloc[1, 1], "TTCG")
+
+        self.assertEqual(self.gg_df.iloc[2, 0], 100)
+        self.assertEqual(self.gg_df.iloc[2, 1], "ATGT")
+
+    def test_parse_degenerate_codon_csv(self):
+        self.assertEqual(self.dc_df.iloc[0, 0], 30)
+        self.assertEqual(self.dc_df.iloc[0, 1], 88)
+        self.assertEqual(self.dc_df.iloc[0, 2], ["A", "S", "T"])
+        self.assertEqual(self.dc_df.iloc[0, 3], [1, 1, 1])
+        self.assertEqual(self.dc_df.iloc[0, 4], "ASC")
+        self.assertEqual(self.dc_df.iloc[0, 5], "GCA")
+
+    def test_find_oligos(self):
+        oligos = find_oligos(self.gg_df)
+        self.assertListEqual(
+            oligos,
+            [
+                (-1, 78),
+                (79, 99),
+                (100, 156),
+                (157, 177),
+                (178, 290),
+                (291, 312),
+                (313, 356),
+            ],
+        )
+
+    def test_find_codons_for_oligo(self):
+        oligos = find_oligos(self.gg_df)
+        oligo_codons = find_codons_for_oligo(oligos[1], self.dc_df)
+        self.assertEqual(
+            oligo_codons,
+            [
+                [(88, "ASC"), (91, "AAA"), (97, "TWT")],
+                [(88, "ASC"), (91, "ACC"), (97, "TWT")],
+                [(88, "ASC"), (91, "CGT"), (97, "TWT")],
+                [(88, "GCA"), (91, "AAA"), (97, "TWT")],
+                [(88, "GCA"), (91, "ACC"), (97, "TWT")],
+                [(88, "GCA"), (91, "CGT"), (97, "TWT")],
+            ],
+        )
+
+    def test_create_dc_oligo(self):
+        oligos = find_oligos(self.gg_df)
+        oligo_codons = find_codons_for_oligo(oligos[1], self.dc_df)
+        oligo_dna = create_dc_oligo(self.dna, oligo_codons[0], oligos[1])
+        self.assertEqual(oligo_dna, "TCGACTTCTASCAAATACTWTCT")
+
+    def test_create_all_dc_oligos(self):
+        create_all_dc_oligos(self.dna, self.gg_df, self.dc_df)
+
+if __name__ == "__main__":
+    unittest.main()
