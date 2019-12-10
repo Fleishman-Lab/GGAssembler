8f46816ac69be9e215a79f4e4b7f7e6eb8abc535
shayho <shayho>
2019-12-10 15:21:45 +0200

    Fixed length based shortest paths

--- dawdlib/golden_gate/dijkstra.py
+++ dawdlib/golden_gate/dijkstra.py
@@ -1,8 +1,17 @@
+from collections import defaultdict
+from functools import partial
 from heapq import heappop, heappush
-from itertools import count
+from itertools import count, repeat
 
 import networkx as nx
 from networkx.algorithms.shortest_paths.weighted import _weight_function
+
+
+def nested_defaultdict(default_factory, depth=1):
+    result = partial(defaultdict, default_factory)
+    for _ in repeat(None, depth - 1):
+        result = partial(defaultdict, result)
+    return result()
 
 
 def all_shortest_paths(
@@ -76,21 +85,22 @@ def all_shortest_paths(
             "Target {} cannot be reached" "from Source {}".format(target, source)
         )
 
-    stack = [[target, 0]]
-    top = 0
-    while top >= 0:
-        node, i = stack[top]
-        if node == source:
-            yield [p for p, n in reversed(stack[: top + 1])]
-        if len(pred[node]) > i:
-            top += 1
-            if top == len(stack):
-                stack.append([pred[node][i], 0])
+    for path_len in pred[target].keys():
+        stack = [[target, 0, path_len]]
+        top = 0
+        while top >= 0:
+            node, i, l = stack[top]
+            if node == source:
+                yield [p for p, n, l in reversed(stack[: top + 1])]
+            if len(pred[node][l]) > i:
+                top += 1
+                if top == len(stack):
+                    stack.append([pred[node][l][i], 0, l - 1])
+                else:
+                    stack[top] = [pred[node][l][i], 0, l - 1]
             else:
-                stack[top] = [pred[node][i], 0]
-        else:
-            stack[top - 1][1] += 1
-            top -= 1
+                stack[top - 1][1] += 1
+                top -= 1
 
 
 def dijkstra_predecessor_and_distance(
@@ -164,7 +174,8 @@ def dijkstra_predecessor_and_distance(
     """
 
     weight = _weight_function(G, weight)
-    pred = {source: []}  # dictionary of predecessors
+    pred = nested_defaultdict(list, 2)  # dictionary of predecessors
+    pred[source][1] = [source]
     return (
         pred,
         _dijkstra(G, source, weight, pred=pred, cutoff=cutoff, len_cutoff=len_cutoff),
@@ -192,230 +203,6 @@ def _dijkstra(
         target=target,
         len_cutoff=len_cutoff,
     )
-
-
-def single_source_dijkstra(
-    G, source, target=None, cutoff=None, len_cutoff=None, weight="weight"
-):
-    """Find shortest weighted paths and lengths from a source node.
-
-    Compute the shortest path length between source and all other
-    reachable nodes for a weighted graph.
-
-    Uses Dijkstra's algorithm to compute shortest paths and lengths
-    between a source and all other reachable nodes in a weighted graph.
-
-    Parameters
-    ----------
-    G : NetworkX graph
-
-    source : node label
-       Starting node for path
-
-    target : node label, optional
-       Ending node for path
-
-    cutoff : integer or float, optional
-       Depth to stop the search. Only return paths with length <= cutoff.
-
-    weight : string or function
-       If this is a string, then edge weights will be accessed via the
-       edge attribute with this key (that is, the weight of the edge
-       joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
-       such edge attribute exists, the weight of the edge is assumed to
-       be one.
-
-       If this is a function, the weight of an edge is the value
-       returned by the function. The function must accept exactly three
-       positional arguments: the two endpoints of an edge and the
-       dictionary of edge attributes for that edge. The function must
-       return a number.
-
-    Returns
-    -------
-    distance, path : pair of dictionaries, or numeric and list.
-       If target is None, paths and lengths to all nodes are computed.
-       The return value is a tuple of two dictionaries keyed by target nodes.
-       The first dictionary stores distance to each target node.
-       The second stores the path to each target node.
-       If target is not None, returns a tuple (distance, path), where
-       distance is the distance from source to target and path is a list
-       representing the path from source to target.
-
-    Raises
-    ------
-    NodeNotFound
-        If `source` is not in `G`.
-
-    Examples
-    --------
-    >>> G = nx.path_graph(5)
-    >>> length, path = nx.single_source_dijkstra(G, 0)
-    >>> print(length[4])
-    4
-    >>> for node in [0, 1, 2, 3, 4]:
-    ...     print('{}: {}'.format(node, length[node]))
-    0: 0
-    1: 1
-    2: 2
-    3: 3
-    4: 4
-    >>> path[4]
-    [0, 1, 2, 3, 4]
-    >>> length, path = nx.single_source_dijkstra(G, 0, 1)
-    >>> length
-    1
-    >>> path
-    [0, 1]
-
-    Notes
-    -----
-    Edge weight attributes must be numerical.
-    Distances are calculated as sums of weighted edges traversed.
-
-    The weight function can be used to hide edges by returning None.
-    So ``weight = lambda u, v, d: 1 if d['color']=="red" else None``
-    will find the shortest red path.
-
-    Based on the Python cookbook recipe (119466) at
-    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/119466
-
-    This algorithm is not guaranteed to work if edge weights
-    are negative or are floating point numbers
-    (overflows and roundoff errors can cause problems).
-
-    See Also
-    --------
-    single_source_dijkstra_path()
-    single_source_dijkstra_path_length()
-    single_source_bellman_ford()
-    """
-    return multi_source_dijkstra(
-        G, {source}, cutoff=cutoff, target=target, weight=weight, len_cutoff=len_cutoff
-    )
-
-
-def multi_source_dijkstra(
-    G, sources, target=None, cutoff=None, len_cutoff=None, weight="weight"
-):
-    """Find shortest weighted paths and lengths from a given set of
-    source nodes.
-
-    Uses Dijkstra's algorithm to compute the shortest paths and lengths
-    between one of the source nodes and the given `target`, or all other
-    reachable nodes if not specified, for a weighted graph.
-
-    Parameters
-    ----------
-    G : NetworkX graph
-
-    sources : non-empty set of nodes
-        Starting nodes for paths. If this is just a set containing a
-        single node, then all paths computed by this function will start
-        from that node. If there are two or more nodes in the set, the
-        computed paths may begin from any one of the start nodes.
-
-    target : node label, optional
-       Ending node for path
-
-    cutoff : integer or float, optional
-       Depth to stop the search. Only return paths with length <= cutoff.
-
-    weight : string or function
-       If this is a string, then edge weights will be accessed via the
-       edge attribute with this key (that is, the weight of the edge
-       joining `u` to `v` will be ``G.edges[u, v][weight]``). If no
-       such edge attribute exists, the weight of the edge is assumed to
-       be one.
-
-       If this is a function, the weight of an edge is the value
-       returned by the function. The function must accept exactly three
-       positional arguments: the two endpoints of an edge and the
-       dictionary of edge attributes for that edge. The function must
-       return a number.
-
-    Returns
-    -------
-    distance, path : pair of dictionaries, or numeric and list
-       If target is None, returns a tuple of two dictionaries keyed by node.
-       The first dictionary stores distance from one of the source nodes.
-       The second stores the path from one of the sources to that node.
-       If target is not None, returns a tuple of (distance, path) where
-       distance is the distance from source to target and path is a list
-       representing the path from source to target.
-
-    Examples
-    --------
-    >>> G = nx.path_graph(5)
-    >>> length, path = nx.multi_source_dijkstra(G, {0, 4})
-    >>> for node in [0, 1, 2, 3, 4]:
-    ...     print('{}: {}'.format(node, length[node]))
-    0: 0
-    1: 1
-    2: 2
-    3: 1
-    4: 0
-    >>> path[1]
-    [0, 1]
-    >>> path[3]
-    [4, 3]
-
-    >>> length, path = nx.multi_source_dijkstra(G, {0, 4}, 1)
-    >>> length
-    1
-    >>> path
-    [0, 1]
-
-    Notes
-    -----
-    Edge weight attributes must be numerical.
-    Distances are calculated as sums of weighted edges traversed.
-
-    The weight function can be used to hide edges by returning None.
-    So ``weight = lambda u, v, d: 1 if d['color']=="red" else None``
-    will find the shortest red path.
-
-    Based on the Python cookbook recipe (119466) at
-    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/119466
-
-    This algorithm is not guaranteed to work if edge weights
-    are negative or are floating point numbers
-    (overflows and roundoff errors can cause problems).
-
-    Raises
-    ------
-    ValueError
-        If `sources` is empty.
-    NodeNotFound
-        If any of `sources` is not in `G`.
-
-    See Also
-    --------
-    multi_source_dijkstra_path()
-    multi_source_dijkstra_path_length()
-
-    """
-    if not sources:
-        raise ValueError("sources must not be empty")
-    if target in sources:
-        return (0, [target])
-    weight = _weight_function(G, weight)
-    paths = {source: [source] for source in sources}  # dictionary of paths
-    dist = _dijkstra_multisource(
-        G,
-        sources,
-        weight,
-        paths=paths,
-        cutoff=cutoff,
-        target=target,
-        len_cutoff=len_cutoff,
-    )
-    if target is None:
-        return (dist, paths)
-    try:
-        return (dist[target], paths[target])
-    except KeyError:
-        raise nx.NetworkXNoPath("No path to {}.".format(target))
 
 
 def _dijkstra_multisource(
@@ -473,9 +260,8 @@ def _dijkstra_multisource(
 
     push = heappush
     pop = heappop
-    dist = {}  # dictionary of final distances
-    length = {}  # dictionary of final lengths
-    seen = {}
+    dist = defaultdict(dict)  # dictionary of final distances
+    seen = defaultdict(dict)
     # fringe is heapq with 4-tuples (distance, length, c,node)
     # use the count c to avoid comparing nodes (may not be able to)
     c = count()
@@ -483,41 +269,40 @@ def _dijkstra_multisource(
     for source in sources:
         if source not in G:
             raise nx.NodeNotFound("Source {} not in G".format(source))
-        seen[source] = 0
+        seen[source][0] = 0
         push(fringe, (0, 0, next(c), source))
     while fringe:
         (d, l, _, v) = pop(fringe)
-        if v in dist:
+        if v in dist and l in dist[v]:
             continue  # already searched this node.
-        dist[v] = d
-        length[v] = l
+        dist[v][l] = d
         if v == target:
             break
         for u, e in G_succ[v].items():
             cost = weight(v, u, e)
             if cost is None:
                 continue
-            vu_dist = dist[v] + cost
+            vu_dist = dist[v][l] + cost
             if cutoff is not None:
                 if vu_dist > cutoff:
                     continue
-            vu_len = length[v] + 1
+            vu_len = l + 1
             if len_cutoff is not None:
                 if vu_len > len_cutoff:
                     continue
-            if u in dist:
-                if vu_dist < dist[u]:
+            if u in dist and vu_len in dist[u]:
+                if vu_dist < dist[u][vu_len]:
                     raise ValueError("Contradictory paths found:", "negative weights?")
-            elif u not in seen or vu_dist < seen[u]:
-                seen[u] = vu_dist
+            elif u not in seen or vu_len not in seen[u] or vu_dist < seen[u][vu_len]:
+                seen[u][vu_len] = vu_dist
                 push(fringe, (vu_dist, vu_len, next(c), u))
                 if paths is not None:
-                    paths[u] = paths[v] + [u]
+                    paths[u][vu_len] = paths[v][l] + [u]
                 if pred is not None:
-                    pred[u] = [v]
-            elif vu_dist == seen[u]:
+                    pred[u][vu_len] = [v]
+            elif vu_dist == seen[u][vu_len]:
                 if pred is not None:
-                    pred[u].append(v)
+                    pred[u][vu_len].append(v)
 
     # The optional predecessor and path dictionaries can be accessed
     # by the caller via the pred and paths objects passed as arguments.
