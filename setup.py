0e9c905747043572fd99951119f30a1f1ffede88
shayho <shayho>
2020-02-11 12:39:46 +0200

    Starting to make this libarary usable for others. This means provinding a better setup and requirments for development

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
 from distutils.extension import Extension
+
+from setuptools import find_packages, setup
+
 import numpy
 
 #
@@ -16,19 +18,20 @@ if USE_CYTHON:
     try:
         from Cython.Build import cythonize
         from Cython.Compiler import Options
+
         Options.docstrings = True
     except ImportError:
-        if USE_CYTHON == 'auto':
+        if USE_CYTHON == "auto":
             USE_CYTHON = False
         else:
             raise
 
-ext = '.pyx' if USE_CYTHON else '.cpp'
+ext = ".pyx" if USE_CYTHON else ".cpp"
 
 ext_modules = [
     Extension(
-        'dawdlib.dijkstra.colorful',
-        ['dawdlib/dijkstra/colorful'+ext],
+        "dawdlib.dijkstra.colorful",
+        ["dawdlib/dijkstra/colorful" + ext],
         include_dirs=[numpy.get_include()],
         # define_macros=[('CYTHON_TRACE', '1'), ('CYTHON_TRACE_NOGIL', '1')]
     )
@@ -45,5 +48,14 @@ setup(
     description="Utils to help create a cost sensitive degenerate codon sequence",
     packages=find_packages(),
     zip_safe=False,
-    ext_modules=ext_modules
+    ext_modules=ext_modules,
+    install_requires=[
+        "networkx",
+        "numpy",
+        "cvxpy",
+        "biopython",
+        "synbiochem-py",
+        "pandas",
+        "fire",
+    ],
 )
