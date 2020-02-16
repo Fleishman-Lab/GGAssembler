6062babb48bed160e1d2d67701e5612947398f93
jonathaw <jonathan.weinstein2012@gmail.com>
2020-02-16 19:45:09 +0200

    add script and corresponding functions to create the to order dataframe from file inputs only

--- dawdlib/create_embl/create_embl.py
+++ dawdlib/create_embl/create_embl.py
@@ -5,18 +5,15 @@ from Bio import SeqFeature, SeqIO
 
 from dawdlib.create_embl.embl_maker import (create_dc_features, create_path_features, df_to_gate_path,
                                             parse_degenerate_codon_csv)
-from dawdlib.golden_gate.utils import parse_dna
 
 
 def main():
     deg_table_file = "/home/labs/fleishman/jonathaw/for_others/200124_lihee/271/deg_table.csv"
     embl_file = "/home/labs/fleishman/jonathaw/for_others/200124_lihee/271/271_2p48.embl"
-    dna_file = ""
     path_file = "/home/labs/fleishman/jonathaw/for_others/200124_lihee/271/271_path_df.csv"
     out_embl = "/home/labs/fleishman/jonathaw/for_others/200124_lihee/271/out.embl"
 
     seq_record = SeqIO.read(embl_file, "embl")
-    # dna = parse_dna(dna_file)
 
     features: List[SeqFeature.SeqFeature] = []
 
