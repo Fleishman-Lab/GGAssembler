5ce8091606b4eb2c04b15ae7eafef1a625663404
hochshi <hochshi@users.noreply.github.com>
2022-03-17 16:55:10 +0200

    Update __init__.py

--- dawdlib/golden_gate/resources/__init__.py
+++ dawdlib/golden_gate/resources/__init__.py
@@ -2,6 +2,6 @@ import glob
 import os
 
 ligation_data = {
-    os.path.basenam(csv): os.path.abspath(csv)
+    os.path.basename(csv): os.path.abspath(csv)
     for csv in glob.glob(os.path.join(os.path.dirname(__file__), "*.csv"))
 }
